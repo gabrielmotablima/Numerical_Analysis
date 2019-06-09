c9099dab5ecc4c9dc39511f1dce7ab907c239b28
Gabriel Mota Bromonschenkel Lima <31813682+Katchau1katchau@users.noreply.github.com>
2019-06-09 01:14:27 -0300

    A little bit of comments

--- Roots_of_Equations/Bisection_Method.java
+++ Roots_of_Equations/Bisection_Method.java
@@ -1,28 +1,32 @@
+// Class about our function used in Algorithm
 public class Function {
 	private double y, x;
 
 	public Function(){
 	}
-
+	
+	// Method to generate the function to be analysed
 	public Function(double x){
 		this.x=x;
 		//this.y = 2.0*x*x*x - 4.0*x*x + 3.0*x;
 		this.y = Math.cos(x) - x;
 	}
-
+	
+	// Main code block
 	public static void main(String args[]) {
-		double a   = 0.0 ;
-		double b   =  1.0 ;
-		double p =  1.0e-8;
-		int count  =  0;
-		double x = (a+b)/2;
+		double a   = 0.0 ; // Boundery to left mean point side
+		double b   =  1.0 ; // Boundery to right mean point side
+		double p =  1.0e-8; // Tolerance parameter
+		int count  =  0; // Iteration's number
+		double x = (a+b)/2; // x mean point
 		Function M;
 		Function Mx = new Function(x);
 
 		System.out.println("#################################################################################");
 		System.out.println("################################### BISECTION ###################################");
 		System.out.println("#################################################################################");
-	
+		
+		// The bisection magic algorithm
 		while(Math.abs((b-a))>p && Math.abs((Mx.y))>p){
 			x = (a+b)/2;
 			Mx = new Function(x);
